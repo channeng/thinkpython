7ff8c6fbabf27cd6cfb7b1b672918464f336b7ed
channeng <shannon@action.io>
2015-08-02 12:13:46 -0700

    ex9.4

---
+++ ex9-4.py
@@ -0,0 +1,22 @@
+def uses_only(word,uses_letters):
+	for letter in word:
+		for i in uses_letters:
+			if letter == i:
+				break
+		else:
+			return False
+	else:
+		return True
+
+with open("ex9-2words.txt") as f:
+	uses_letters = raw_input("Please enter uses_letters in a string \n > ")
+	total =0
+	counter =0
+	for line in f:
+		word= line.strip()
+		total +=1
+		if uses_only(word,uses_letters):
+			counter +=1
+			print word
+	percent =counter/float(total)
+	print percent
